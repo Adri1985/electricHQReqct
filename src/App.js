b7e4165d91221a2014d6cc270f2f85eb9a711ffe
Adrian Campo <adrian.campo1iclodud.com>
2022-07-15 11:01:51 -0300

    corrigiendo repo react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import NavBar from './components/NavBar/NavBar';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+        <NavBar />
+        <header className="App-header">
+        <p>Hello World</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
