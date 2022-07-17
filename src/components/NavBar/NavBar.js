b312271a560fe0234d41dc247c30d6127ff81bd0
Adrian Campo <adrian.campo1iclodud.com>
2022-07-17 16:28:49 -0300

    entrega segundo desafiio, NavBar.js

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
   return (
     <Navbar bg="dark" variant="dark">
     <Container>
-        <Navbar.brand href ="#home">Navbar</Navbar.brand>
+        <Navbar.Brand href ="#home">Navbar</Navbar.Brand>
         <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#products">Products</Nav.Link>
